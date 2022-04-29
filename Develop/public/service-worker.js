48ea3c05660302bae5c4362e43017d104e09e58a
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-04-28 23:21:38 -0700

    added service worker activate

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -29,6 +29,22 @@ self.addEventListener('install', function(e) {
     );
     self.skipWaiting();
 })
-//Activate Service Worker
 
+//Activate Service Worker
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(keyList => {
+            return Promise.all(
+                keyList.map(key => {
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Removing old cache data', key);
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+
+    self.clients.claim();
+});
 //Intercept Fetch

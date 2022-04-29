6d2fb912ee986f7c2579eebbfe803bad84dc4b98
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-04-28 23:19:41 -0700

    added cache names and files

---
+++ Develop/public/service-worker.js
@@ -0,0 +1,26 @@
+// Cache names
+const CACHE_NAME = 'budget-tracker-cache';
+const DATA_CACHE_NAME = 'data-cache-budget';
+
+//Connect files to cache
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/manifest.json',
+    '/js/index.js',
+    '/css/styles.css',
+    '/icons/icon-72x72.png',
+    '/icons/icon-96x96.png',
+    '/icons/icon-128x128.png',
+    '/icons/icon-144x144.png',
+    '/icons/icon-152x152.png',
+    '/icons/icon-192x192.png',
+    '/icons/icon-384x382.png',
+    '/icons/icon-512x512.png'
+];
+
+//Install service worker
+
+//Activate Service Worker
+
+//Intercept Fetch
